4787b2c43f9f64270dd6b7d6835bd7824b9de485
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-03-19 17:05:19 -0500

    Entered in predicted values and computations

--- src/TestExpressions.java
+++ src/TestExpressions.java
@@ -6,14 +6,14 @@
 
 /*
  	Your predicted values for the expressions
-	1.
-	2.
-	3.
-	4.
-	5.
-	6.
-	7.
-	8.
+	1. 8   True
+	2. 11  True
+	3. 12  True
+	4. 4   True
+	5. 3   False. I evaluated in the wrong order
+	6. 11  True
+	7. "2 + 2" + 7   False. I did not concatinate the numbers together   
+	8. 7 + " 2 + 2"  False. I did not concatinate the numbers together
  
  	After testing the expressions in the main method, indicate above if your predictions were correct.
 */
@@ -21,7 +21,7 @@
 public class TestExpressions {
 
 	public static void main(String[] args) {
-		System.out.println("YOUR_TEST_EXPRESSION");
+		System.out.println(3 + 4 + " 2 + 2");
 	}
 
 }
